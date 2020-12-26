2ef8b1b6e906cd56175f856601218c69b245acc9
SeitaHigashi <higashi110902@gmail.com>
2020-12-26 11:24:40 +0900

    add Reviecer

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,8 @@ import traceback
 Radnes = commands.Bot(command_prefix='/')
 Radnes_token = os.environ['RADNES_TOKEN']
 
+Reviecer = commands.Bot(command_prefix='/')
+Reviecer_token = os.environ['REVIECER_TOKEN']
 
 @Radnes.event
 async def on_command_error(ctx, error):
@@ -12,10 +14,20 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+@Reviecer.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
 @Radnes.command()
 async def ping(ctx):
     await ctx.send('pong')
 
+@Reviecer.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
 
 Radnes.run(Radnes_token)
+Reviecer.run(Radnes_token)
