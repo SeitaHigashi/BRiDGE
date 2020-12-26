e244b0ff7b121acc8be88a6e2575e8360c8f1283
SeitaHigashi <higashi110902@gmail.com>
2020-12-26 14:44:02 +0900

    threading test push

--- discordbot.py
+++ discordbot.py
@@ -1,33 +1,39 @@
 from discord.ext import commands
 import os
 import traceback
+import threading
 
-Radnes = commands.Bot(command_prefix='/')
-Radnes_token = os.environ['RADNES_TOKEN']
+radnes = commands.Bot(command_prefix='/')
+radnes_token = os.environ['RADNES_TOKEN']
 
-Reviecer = commands.Bot(command_prefix='/')
-Reviecer_token = os.environ['REVIECER_TOKEN']
+reviecer = commands.Bot(command_prefix='/')
+reviecer_token = os.environ['REVIECER_TOKEN']
 
-@Radnes.event
+@radnes.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@Reviecer.event
+@reviecer.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@Radnes.command()
+@radnes.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-@Reviecer.command()
+@reviecer.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 
-#Radnes.run(Radnes_token)
-Reviecer.run(Reviecer_token)
+radnes_thread = threading.Thread(target=radnes.run, args=(radnes_token))
+reviecer_thread = threading.Thread(target=reviecer.run, args=(reviecer_token))
+
+radnes_thread.start()
+reviecer_thread.start()
+radnes_thread.join()
+reviecer_thread.join()
