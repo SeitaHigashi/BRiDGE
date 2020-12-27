b24d2abfd62f76729c21089fa4a8ab4b1ccd5655
SeitaHigashi <higashi110902@gmail.com>
2020-12-27 19:32:51 +0900

    fix

--- discordbot.py
+++ discordbot.py
@@ -12,10 +12,8 @@ reviecer_token = os.environ['REVIECER_TOKEN']
 
 @radnes.event
 async def on_message(message):
-    if message.author.bot:
-        return
-    if message.content == "/ping":
-        await message.channel.send("pong")
+    if message.content == '/ping':
+        await message.channel.send('pong')
 
 radnes.run(radnes_token)
 
