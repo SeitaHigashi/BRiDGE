eecb3b959960b167241dbb62820e9a3047f8bf97
SeitaHigashi <higashi110902@gmail.com>
2020-12-27 19:38:15 +0900

    fix

--- discordbot.py
+++ discordbot.py
@@ -1,19 +1,19 @@
 import discord
 import sys
 import os
-import traceback
-import threading
+# import traceback
+# import threading
 
 radnes = discord.Client()
 radnes_token = os.environ['RADNES_TOKEN']
 
-#reviecer = discord.Client()
-#reviecer_token = os.environ['REVIECER_TOKEN']
+# reviecer = discord.Client()
+# reviecer_token = os.environ['REVIECER_TOKEN']
 
-#@radnes.event
-#async def on_message(message):
-    #if message.content == '/ping':
-        #await message.channel.send('pong')
+# @radnes.event
+# async def on_message(message):
+    # if message.content == '/ping':
+        # await message.channel.send('pong')
 
 radnes.run(radnes_token)
 
